fbad0247fd2e2edb01a20e3336a1c96e23da6f77
soohyunme <soohyun.dev@gmail.com>
2023-01-02 02:36:49 +0000

    Update unzip.sh
    fix: space included path

--- script/preprocess/unzip_foreigner_speech.sh
+++ script/preprocess/unzip_foreigner_speech.sh
@@ -7,13 +7,12 @@
 #   |-------2.Validation
 
 # Put your absolute 'RootDir' path below
-ROOT_DIR=/code/gitRepo/data/aihub/Foreigner_speech ## it is dummy, please modify it
+ROOT_DIR='/code/gitRepo/data/aihub/Foreigner_speech' ## it is dummy, please modify it
 
 # Put your absolute destination path
-DESTINATION=/path/to/unzip/data ## it is dummy, please modify it
-
+DESTINATION='/path/to/unzip/data' ## it is dummy, please modify it
 
 ## Run unzip code
 python preprocess/unzip.py \
-    ROOT_DIR \
-    --dest $DESTINATION 
+    "$ROOT_DIR" \
+    --dest "$DESTINATION"
