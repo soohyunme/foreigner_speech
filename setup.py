0b48e4a3ec54d04568db43e97c8079a6d3bb76dc
soohyunme <soohyun.dev@gmail.com>
2023-01-02 00:06:47 +0000

    feat: modified setup

--- setup.py
+++ setup.py
@@ -57,121 +57,23 @@ class NumpyExtension(Extension):
 
 
 extensions = [
-    Extension(
-        "fairseq.libbleu",
-        sources=[
-            "fairseq/clib/libbleu/libbleu.cpp",
-            "fairseq/clib/libbleu/module.cpp",
-        ],
-        extra_compile_args=extra_compile_args,
-    ),
     NumpyExtension(
         "fairseq.data.data_utils_fast",
         sources=["fairseq/data/data_utils_fast.pyx"],
         language="c++",
         extra_compile_args=extra_compile_args,
-    ),
-    NumpyExtension(
-        "fairseq.data.token_block_utils_fast",
-        sources=["fairseq/data/token_block_utils_fast.pyx"],
-        language="c++",
-        extra_compile_args=extra_compile_args,
-    ),
+    )
 ]
-
-
-extensions.extend(
-    [
-        cpp_extension.CppExtension(
-            "fairseq.libbase",
-            sources=[
-                "fairseq/clib/libbase/balanced_assignment.cpp",
-            ],
-        ),
-        cpp_extension.CppExtension(
-            "fairseq.libnat",
-            sources=[
-                "fairseq/clib/libnat/edit_dist.cpp",
-            ],
-        ),
-        cpp_extension.CppExtension(
-            "alignment_train_cpu_binding",
-            sources=[
-                "examples/operators/alignment_train_cpu.cpp",
-            ],
-        ),
-    ]
-)
-if "CUDA_HOME" in os.environ:
-    extensions.extend(
-        [
-            cpp_extension.CppExtension(
-                "fairseq.libnat_cuda",
-                sources=[
-                    "fairseq/clib/libnat_cuda/edit_dist.cu",
-                    "fairseq/clib/libnat_cuda/binding.cpp",
-                ],
-            ),
-            cpp_extension.CppExtension(
-                "fairseq.ngram_repeat_block_cuda",
-                sources=[
-                    "fairseq/clib/cuda/ngram_repeat_block_cuda.cpp",
-                    "fairseq/clib/cuda/ngram_repeat_block_cuda_kernel.cu",
-                ],
-            ),
-            cpp_extension.CppExtension(
-                "alignment_train_cuda_binding",
-                sources=[
-                    "examples/operators/alignment_train_kernel.cu",
-                    "examples/operators/alignment_train_cuda.cpp",
-                ],
-            ),
-        ]
-    )
-
-cmdclass = {"build_ext": cpp_extension.BuildExtension}
-
-if "READTHEDOCS" in os.environ:
-    # don't build extensions when generating docs
-    extensions = []
-    if "build_ext" in cmdclass:
-        del cmdclass["build_ext"]
-
-    # use CPU build of PyTorch
-    dependency_links = [
-        "https://download.pytorch.org/whl/cpu/torch-1.7.0%2Bcpu-cp36-cp36m-linux_x86_64.whl"
-    ]
-else:
-    dependency_links = []
-
-
-if "clean" in sys.argv[1:]:
-    # Source: https://bit.ly/2NLVsgE
-    print("deleting Cython files...")
-
-    subprocess.run(
-        ["rm -f fairseq/*.so fairseq/**/*.so fairseq/*.pyd fairseq/**/*.pyd"],
-        shell=True,
-    )
-
-
-extra_packages = []
-if os.path.exists(os.path.join("fairseq", "model_parallel", "megatron", "mpu")):
-    extra_packages.append("fairseq.model_parallel.megatron.mpu")
-
 
 def do_setup(package_data):
     setup(
-        name="fairseq",
+        name="foreigner_speech",
         version=version,
-        description="Facebook AI Research Sequence-to-Sequence Toolkit",
-        url="https://github.com/pytorch/fairseq",
+        description="Foreigner Speech Recognition Library",
+        url="https://github.com/soohyunme/foreigner_speech",
         classifiers=[
-            "Intended Audience :: Science/Research",
-            "License :: OSI Approved :: MIT License",
-            "Programming Language :: Python :: 3.6",
-            "Programming Language :: Python :: 3.7",
-            "Programming Language :: Python :: 3.8",
+            "Intended Audience :: Developers",
+            "Programming Language :: Python :: 3",
             "Topic :: Scientific/Engineering :: Artificial Intelligence",
         ],
         long_description=readme,
@@ -191,25 +93,15 @@ def do_setup(package_data):
             "scikit-learn",
             "packaging",
         ],
-        extras_require={
-            "dev": ["flake8", "pytest", "black==22.3.0"],
-            "docs": ["sphinx", "sphinx-argparse"],
-        },
-        dependency_links=dependency_links,
+        dependency_links=[],
         packages=find_packages(
             exclude=[
                 "examples",
                 "examples.*",
-                "scripts",
-                "scripts.*",
-                "tests",
-                "tests.*",
             ]
-        )
-        + extra_packages,
+        ),
         package_data=package_data,
         ext_modules=extensions,
-        test_suite="tests",
         entry_points={
             "console_scripts": [
                 "fairseq-eval-lm = fairseq_cli.eval_lm:cli_main",
@@ -222,7 +114,6 @@ def do_setup(package_data):
                 "fairseq-validate = fairseq_cli.validate:cli_main",
             ],
         },
-        cmdclass=cmdclass,
         zip_safe=False,
     )
 
