0e726cbffc5737ab212f8ff413fdf1baab339251
soohyunme <soohyun.dev@gmail.com>
2023-01-04 04:47:35 +0000

    refactor: fix typo

--- script/preprocess/unzip_foreigner_speech.sh
+++ script/preprocess/unzip_foreigner_speech.sh
@@ -19,7 +19,7 @@
 #             |----------VS_2. 영어.zip
 
 # Put your absolute 'RootDir' path below
-ROOT_DIR='/code/gitRepo/data/aihub/Foreigner_speech' ## it is dummy, please modify it
+ROOT_DIR='/code/gitRepo/data/aihub/foreigner_speech' ## it is dummy, please modify it
 
 # Put your absolute destination path
 DESTINATION='/path/to/unzip/data' ## it is dummy, please modify it
